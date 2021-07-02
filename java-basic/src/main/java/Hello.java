b1194a1e9676a08dc2291b61f604b207d5617882
jo-sol <zoixssm@gmail.com>
2021-07-02 18:52:18 +0900

    07.02

--- java-basic/src/main/java/Hello.java
+++ java-basic/src/main/java/Hello.java
@@ -7,6 +7,5 @@ class Hello{
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("안녕하세요!");
-        System.out.println("반갑습니다!");
     }
 }
