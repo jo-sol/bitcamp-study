6352ce96fda136d9632ff92603784dc9d34bc742
jo-sol <zoixssm@gmail.com>
2021-07-06 23:21:26 +0900

    '0706'

--- study-test01/app/src/main/java/com/eomcs/pms/App0505.java
+++
@@ -1,7 +0,0 @@
-package com.eomcs.pms;
-
-public class App0505 {
-  public static void main(String[] args) {
-    System.out.print("안녕!");
-  }
-}
---
+++ study-test01/app/src/main/java/com/eomcs/pms/Test.java
@@ -0,0 +1,17 @@
+package com.eomcs.pms;
+
+public class Test {
+
+  public static void main(String[] args) {
+    char a = 'A';
+    char b = 'B';
+    char c = 'C';
+    a = 44032;
+    b = 44033;
+    c = 44034;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+
+}
