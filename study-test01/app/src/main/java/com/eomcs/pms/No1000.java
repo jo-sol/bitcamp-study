504a54785ad8323c06d759ede9c2064b0d500b44
jo-sol <zoixssm@gmail.com>
2021-07-08 09:51:45 +0900

    0708

--- study-test01/app/src/main/java/com/eomcs/pms/No1000.java
+++ study-test01/app/src/main/java/com/eomcs/pms/No1000.java
@@ -1,16 +1,17 @@
 package com.eomcs.pms;
 
-import java.util.Scanner;
-
 public class No1000 {
 
   public static void main(String[] args) {
 
-    Scanner in = new Scanner(System.in); //System.in은 입력한 값을 Byte 단위로 읽는 것
-    int A = in.nextInt(); //in.next()는 문자열 입력 시 공백 전까지만 받는 걸 의미한다
-    int B = in.nextInt();
+    java.util.Scanner keyboard = new java.util.Scanner(System.in);
+    //System.in은 입력한 값을 Byte 단위로 읽는 것
+    int A = keyboard.nextInt(); //in.next()는 문자열 입력 시 공백 전까지만 받는 걸 의미한다
+    int B = keyboard.nextInt();
 
     System.out.print(A+B);
+
+    keyboard.close();
 
   }
 
