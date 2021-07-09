f61a243679892cd875d4f597365beedde140cef3
jo-sol <zoixssm@gmail.com>
2021-07-09 18:30:50 +0900

    0709

--- study-test01/app/src/main/java/com/eomcs/pms/Test.java
+++ study-test01/app/src/main/java/com/eomcs/pms/Test.java
@@ -3,15 +3,11 @@ package com.eomcs.pms;
 public class Test {
 
   public static void main(String[] args) {
-    char a = 'A';
-    char b = 'B';
-    char c = 'C';
-    a = 44032;
-    b = 44033;
-    c = 44034;
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
+    short s1 = 100;
+    short s2 = 200;
+    System.out.println(s1 + "+" + s2 + "=" + (s1 + s2));
+    System.out.println(s1 + "+" + s2 + "=" + s1 + s2);
+    System.out.println(s1 + "+" + s2);
   }
 
 }
